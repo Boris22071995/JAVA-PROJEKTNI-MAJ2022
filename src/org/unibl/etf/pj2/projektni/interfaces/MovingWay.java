8ddd985dbb14c589022bfa690a57eea7d0228c37
Boris22071995 <bvasiljevic21@gmail.com>
2022-06-30 15:19:18 +0200

    Changed interface

---
+++ src/org/unibl/etf/pj2/projektni/interfaces/MovingWay.java
@@ -0,0 +1,5 @@
+package org.unibl.etf.pj2.projektni.interfaces;
+
+public interface MovingWay {
+    String move();
+}
--- src/org/unibl/etf/pj2/projektni/interfejsi/NacinKretanja.java
+++
@@ -1,6 +0,0 @@
-package org.unibl.etf.pj2.projektni.interfejsi;
-
-public interface NacinKretanja {
-
-    public String krecemSe();
-}
